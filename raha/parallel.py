d1dbc4613d2cbbdf506a0ec288365c71f1b452e4
yimlyim <yusufmandirali@web.de>
2023-05-30 23:56:06 +0200

    Add file to coordinate raha/baran in parallel using dask

--- raha/parallel.py
+++ raha/parallel.py
@@ -1,4 +1,4 @@
-from raha import *
+import raha.dataset
 import os
 import sys
 
@@ -9,14 +9,20 @@ if __name__ == "__main__":
     client = Client(cluster)
 
 
-    dataset_name = "flights"
-    dataset_dictionary = {
-        "name": dataset_name,
-        "path": os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "datasets", dataset_name, "dirty.csv")),
-        "clean_path": os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "datasets", dataset_name, "clean.csv"))
+    dataset_dict = {
+        "name": "movies_1",
+        "path": "datasets/movies_1/dirty.csv",
+        "clean_path": "datasets/movies_1/clean.csv"
     }
+    dset = raha.dataset.Dataset(dataset_dict)
+    dframe = dset.dataframe
+    print(dframe)
 
-    data = raha.dataset.Dataset(dataset_dictionary)
-    print(data.dataframe)
+    cluster = LocalCluster(n_workers=4, threads_per_worker=8)
+    client = Client(cluster)
+    dframe = client.persist(dframe)
+    
+    print(dframe.compute())
+
 
     print("Hello World!")
