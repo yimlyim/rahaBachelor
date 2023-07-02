a313ee8445f5bdce459b3723a2ee75920aee11ab
yimlyim <yusufmandirali@web.de>
2023-07-02 17:45:08 +0200

    Refactoring run_strategies and moved utility functions out to dataset_parallel.py

--- raha/detection_parallel.py
+++ raha/detection_parallel.py
@@ -39,6 +39,7 @@ class DetectionParallel:
     def __init__(self):
         self.LABELING_BUDGET = 20
         self.USER_LABELING_ACCURACY = 1.0
+        self.BENCHMARK = True
         self.VERBOSE = False
         self.SAVE_RESULTS = True
         self.CLUSTERING_BASED_SAMPLING = True
@@ -49,6 +50,28 @@ class DetectionParallel:
                                            RULE_VIOLATION_DETECTION, KNOWLEDGE_BASE_VIOLATION_DETECTION]
         self.HISTORICAL_DATASETS = []
     
+
+    @staticmethod
+    def create_shared_dataset(dataset):
+        pickled_dataset = pickle.dumps(dataset, protocol=pickle.HIGHEST_PROTOCOL)
+        pickled_dataset_size = len(pickled_dataset)
+        shared_mem_area = sm.SharedMemory(name=dataset.own_mem_ref, create=True, size=pickled_dataset_size)
+        shared_mem_area.buf[:pickled_dataset_size] = pickled_dataset
+
+        shared_mem_area.close()
+        del shared_mem_area
+        return
+    
+    @staticmethod
+    def load_shared_dataset(dataset_ref):
+        shared_mem_area = sm.SharedMemory(name=dataset_ref, create=False)
+        deserialized_dataset = pickle.loads(shared_mem_area.buf)
+
+        shared_mem_area.close()
+        del shared_mem_area
+        return deserialized_dataset
+
+
     @staticmethod
     def create_shared_dataframe(dataframe_filepath, mem_area_name):
         """
@@ -65,29 +88,29 @@ class DetectionParallel:
 
         #Read DataFrame in parallel
         kwargs = {'sep': ',', 'header':'infer', 'encoding':'utf-8', 'dtype': str, 'keep_default_na': False, 'low_memory': False}
-        dataset = dask.dataframe.read_csv(urlpath=dataframe_filepath, blocksize=int(blocksize), **kwargs).applymap(dset.Dataset.value_normalizer)
-        dataset = client.compute(dataset).result()
+        dataframe = dask.dataframe.read_csv(urlpath=dataframe_filepath, blocksize=int(blocksize), **kwargs).applymap(dset.Dataset.value_normalizer)
+        dataframe = client.compute(dataframe).result()
 
-        pickled_dataset = pickle.dumps(dataset, protocol=pickle.HIGHEST_PROTOCOL)
-        pickled_dataset_size = len(pickled_dataset)
-        print("Size of pickled dataframe " + str(pickled_dataset_size))
+        pickled_dataframe = pickle.dumps(dataframe, protocol=pickle.HIGHEST_PROTOCOL)
+        pickled_dataframe_size = len(pickled_dataframe)
+        print("Size of pickled dataframe " + str(pickled_dataframe_size))
 
-        shared_mem_area = sm.SharedMemory(name=mem_area_name, create=True, size=pickled_dataset_size)
-        shared_mem_area.buf[:pickled_dataset_size] = pickled_dataset
+        shared_mem_area = sm.SharedMemory(name=mem_area_name, create=True, size=pickled_dataframe_size)
+        shared_mem_area.buf[:pickled_dataframe_size] = pickled_dataframe
 
         shared_mem_area.close()
         del shared_mem_area
         return mem_area_name
 
     @staticmethod
-    def create_shared_split_dataset(dataframe_ref):
+    def create_shared_split_dataframe(dataframe_ref):
         dataframe = DetectionParallel.load_shared_dataframe(dataframe_ref)
 
         for column in dataframe.columns.tolist():
-            pickled_dataset = pickle.dumps(dataframe[column], protocol=pickle.HIGHEST_PROTOCOL)
-            pickled_dataset_size = len(pickled_dataset)
-            shared_mem_area = sm.SharedMemory(name=column, create=True, size=pickled_dataset_size)
-            shared_mem_area.buf[:pickled_dataset_size] = pickled_dataset
+            pickled_dataframe = pickle.dumps(dataframe[column], protocol=pickle.HIGHEST_PROTOCOL)
+            pickled_dataframe_size = len(pickled_dataframe)
+            shared_mem_area = sm.SharedMemory(name=column, create=True, size=pickled_dataframe_size)
+            shared_mem_area.buf[:pickled_dataframe_size] = pickled_dataframe
             shared_mem_area.close()
             del shared_mem_area
         return
@@ -113,13 +136,14 @@ class DetectionParallel:
         return {}
         
     #Todo
-    def run_pattern_strategy(self, configuration, dataframe_ref, dataframe_filepath):
+    def run_pattern_strategy(self, configuration, dataset_ref):
         """
         """
         outputted_cells = {}
+        dataset = DetectionParallel.load_shared_dataset(dataset_ref)
         column_name, character = configuration
         dataframe = DetectionParallel.load_shared_dataframe(column_name)
-        j = DetectionParallel.get_column_names(dataframe_filepath).index(column_name)
+        j = DetectionParallel.get_column_names(dataset.dirty_path).index(column_name)
 
         for i, value in dataframe.items():
             try:
@@ -177,7 +201,7 @@ class DetectionParallel:
         return strategy_results
     
     @staticmethod
-    def setup_outlier_metadata(dataframe_ref):
+    def setup_outlier_metadata(dataset_ref):
         """
         Worker-Process in a parallel manner. Creates Gaussian configuration and Histogram configuration and return them.
         """
@@ -191,11 +215,11 @@ class DetectionParallel:
                         list(itertools.product(["gaussian"],
                                                 ["1.0", "1.3", "1.5", "1.7", "2.0", "2.3", "2.5", "2.7", "3.0"]))]
 
-        configurations.extend([dataframe_ref, OUTLIER_DETECTION, conf] for conf in cartesian_config)
+        configurations.extend([dataset_ref, OUTLIER_DETECTION, conf] for conf in cartesian_config)
         return configurations
 
     @staticmethod
-    def pattern_violation_worker(dataframe_ref, column_name, dataframe_filepath):
+    def pattern_violation_worker(dataset_ref, column_name):
         """
         Worker-Process in a parallel manner. Extracts all characters of one specific column and returns them.
         """
@@ -211,25 +235,25 @@ class DetectionParallel:
         character_dict = {character: 1 for character in column_data} 
         character_dict_list = [[column_name, character] for character in character_dict]  
 
-        configurations.extend([dataframe_ref, PATTERN_VIOLATION_DETECTION, conf] for conf in character_dict_list)
+        configurations.extend([dataset_ref, PATTERN_VIOLATION_DETECTION, conf] for conf in character_dict_list)
         del dataset_column, character_dict, column_data
         return configurations
 
     @staticmethod
-    def setup_pattern_violation_metadata(dataframe_ref, dataframe_filepath):
+    def setup_pattern_violation_metadata(dataset_ref):
         """
         Calculates Meta-Data for pattern-violation application later on
         """
         futures = []
         configurations = []
         client = get_client()
-        column_names = DetectionParallel.get_column_names(dataframe_filepath)
+        dataset = DetectionParallel.load_shared_dataset(dataset_ref)
+        column_names = DetectionParallel.get_column_names(dataset.dirty_path)
         
         #Call a worker for each column name
-        arguments1 = [dataframe_ref] * len(column_names)
+        arguments1 = [dataset_ref] * len(column_names)
         arguments2 = [column_name  for column_name in column_names]
-        arguments3 = [dataframe_filepath] * len(column_names)
-        futures.append(client.map(DetectionParallel.pattern_violation_worker, arguments1, arguments2, arguments3))
+        futures.append(client.map(DetectionParallel.pattern_violation_worker, arguments1, arguments2))
 
         #Gather results and merge "list of lists" into one big list
         results_unmerged = client.gather(futures=futures, direct=True)[0]
@@ -237,22 +261,23 @@ class DetectionParallel:
 
         return results
     @staticmethod
-    def setup_rule_violation_metadata(dataframe_ref, dataframe_filepath):
+    def setup_rule_violation_metadata(dataset_ref):
         """
         Calculates Meta-Data for rule-violation application later on.
         This method just creates pairs of column names as potential FDs
         """
         configurations = []
-        column_names = DetectionParallel.get_column_names(dataframe_filepath)
+        dataset = DetectionParallel.load_shared_dataset(dataset_ref)
+        column_names = DetectionParallel.get_column_names(dataset.dirty_path)
         column_pairs = [[col1, col2] for (col1,col2) in itertools.product(column_names, column_names) if col1 != col2]
-        configurations.extend([[dataframe_ref, RULE_VIOLATION_DETECTION, column_pair] for column_pair in column_pairs])
+        configurations.extend([[dataset_ref, RULE_VIOLATION_DETECTION, column_pair] for column_pair in column_pairs])
 
         return configurations
     @staticmethod
-    def setup_knowledge_violation_metadata(dataframe_ref, dataframe_filepath):
+    def setup_knowledge_violation_metadata(dataset_ref):
         configurations = []
         paths = [os.path.join(os.path.dirname(__file__), "tools", "KATARA", "knowledge-base", path) for path in os.listdir(os.path.join(os.path.dirname(__file__), "tools", "KATARA", "knowledge-base"))]
-        configurations.extend([[dataframe_ref, KNOWLEDGE_BASE_VIOLATION_DETECTION, path] for path in paths])
+        configurations.extend([[dataset_ref, KNOWLEDGE_BASE_VIOLATION_DETECTION, path] for path in paths])
 
         return configurations
 
@@ -269,20 +294,20 @@ class DetectionParallel:
         for algorithm_name in self.ERROR_DETECTION_ALGORITHMS:
             match algorithm_name:
                 case constants.OUTLIER_DETECTION:
-                    futures.append(client.submit(DetectionParallel.setup_outlier_metadata, dataset.dirty_ref))
+                    futures.append(client.submit(DetectionParallel.setup_outlier_metadata, dataset.own_mem_ref))
                 case constants.PATTERN_VIOLATION_DETECTION:
-                    futures.append(client.submit(DetectionParallel.setup_pattern_violation_metadata, dataset.dirty_ref, dataset.path))
+                    futures.append(client.submit(DetectionParallel.setup_pattern_violation_metadata, dataset.own_mem_ref))
                 case constants.RULE_VIOLATION_DETECTION:
-                    futures.append(client.submit(DetectionParallel.setup_rule_violation_metadata, dataset.dirty_ref, dataset.path))
+                    futures.append(client.submit(DetectionParallel.setup_rule_violation_metadata, dataset.own_mem_ref))
                 case constants.KNOWLEDGE_BASE_VIOLATION_DETECTION:
-                    futures.append(client.submit(DetectionParallel.setup_knowledge_violation_metadata, dataset.dirty_ref, dataset.path))
+                    futures.append(client.submit(DetectionParallel.setup_knowledge_violation_metadata, dataset.own_mem_ref))
                 case _:
                     raise ValueError("Algorithm " + str(algorithm) + " is not supported!")
         
         
         results = list(itertools.chain.from_iterable(client.gather(futures=futures, direct=True)))
         endtime = time.time()
-        print(results)
+        #print(results)
         print("Raha strategy metadata generation(parallel): "+  str(endtime - starttime))
         #Start Detecting Errors in parallel
         #futures = client.map(self.parallel_strat_runner_process, results)
